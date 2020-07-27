ec4f36ccce19486d848c73119c2523e3c6c2f437
alex-heritier <alex.heritier@gmail.com>
2020-07-27 14:09:54 -0700

    trying out new colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chinese_fluent/screen/home.dart';
+import 'package:chinese_fluent/value/styles.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ChineseFluent());
@@ -11,16 +12,9 @@ class ChineseFluent extends StatelessWidget {
       title: 'Chinese Fluent',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+//        primarySwatch: Colors.red, // Colors.blue,
+//        primaryColor: Styles.THEME_RED,
       ),
       home: Home(),
     );
---
+++ lib/value/styles.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+abstract class Styles {
+  static const Color THEME_RED = Color(0xffdd3956); // TikTok red
+}
